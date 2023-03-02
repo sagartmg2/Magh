3b7ef59156b934f1ef71b71d64b9ccf64555c084
Sagar <dev.sagartmg@gmail.com>
2023-03-02 17:11:17 +0545

    init javascript

---
+++ js/index.js
@@ -0,0 +1,93 @@
+
+/*
+    variables  // container // box
+        <varaible_name> = <value>
+ */
+
+/**/
+
+
+prefix_sentence = 'we have course' // String variable
+course1 = "MERN STACK"
+
+price = 15000 // Number variable
+
+// 1course = "one" 
+
+// cmp_name = "Mindrisers" // snake_case
+// companyName = "Mindrisers" // camelCase
+
+
+companyName = "Mindrisers" //  initialization 
+companyName = "Mindrisers Consortium pvt ltd" // re-initalizaiton 
+
+
+var brand = "samsung"  //  till 2015 
+// ecamscript   es6 - 2015 
+let color = "black"
+const PI = 3.14 // constant varaible -- cannot be re-initizaed. 
+
+
+var dimension; // variable declartion 
+dimension = "3d" // initaliastion
+dimension = "4d" // re-initaliastion
+
+color = "orange" // re-initilaziton..
+
+// PI = 3.12323
+
+
+
+
+var company = "mindrisers" // decr and init
+var company = "mindrisers constorimum" // decleartion and intitalization both
+company = "mindrisers constorimum pvt ltd"  // re-inital
+
+console.log(company);
+
+let activeStatus = true; // declation and intiatl
+// let  activeStatus = false;   // cannot re-declrate let 
+activeStatus = false
+console.log("activeStatus", activeStatus);
+
+
+// let a = "Ram"
+
+
+
+/*
+    for = "changed"  // for is a reserved word // we cannot make varaibles from resrve word
+    boolean = true;
+ */
+
+
+// console.log(" Hello World ! ");
+// console.log(" we have course " + course1 + "and price is " + price);
+// console.log(" we have course Flutter")
+// console.log(" we have course QA")
+// console.log(prefix_sentence + "  Marketing")
+// console.log(prefix_sentence + " Graphic Design  ")
+// console.log("we are currently in " + course1);
+
+/* DRY - donot repeat yourself */
+
+
+/* 
+    print ("hello wrodl ")
+    System.out.printLn("Hello world");
+    print_r("");
+ */
+
+/* 
+    DATA TYPES
+        text -> String
+        number -> Number
+            -> Integer 
+            -> Float / Double / Decimal 
+        true, false -> Boolean 
+*/
+// prompt()
+
+console.log(12 * 10);
+
+// console.log("area of reactangle having length 12 and width 2 is 24");
