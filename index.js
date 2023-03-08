af676ded6849a2c812629fda5eca3bcb9a5723c4
Sagar <dev.sagartmg@gmail.com>
2023-03-08 10:34:12 +0545

    revise data-types

--- index.js
+++ index.js
@@ -1,137 +1,63 @@
 
-
-let name; // 
-nmae = "js"
-
-let age = 10;
-let status = true
-
-
-let course;
-console.log("couse is " + course);
-
-/*  primiteive  Data types
-    String
-    Number 
-    Boolean
-    undefined
-    null
-*/
+let temp = "temporary variable"
+let arr = []
 
 /* 
-    non-primitive data types // collection // reference data types
-    - Array
-    - Object 
-*/
-
-let data = null; // explicitely defining value as none  , no existance
-console.log(data);
-
-
-let brand1 = "puma"
-let brand2 = "levis"
-let brand3 = "adidas"
-
-
-
-// let brands = "puma", "levis"  xx this is not possible
-// let ages =  50, 50 //  xx this is not possible
-
-/* 
-    Array
-        - collection of different values
-        - usually it is a collection of similar data types
-
-        elements
-        index  // always start with 0
-
-        syntax 
-        let <array_name> = [ <value>,<value>,<value>,......  ]
-
-        read as
-        let <array_name> = [ 1stelement, 2ndelement, ...........  ]
-        or
-        let <array_name> = [0thindex, 1stindex, 2ndindex, ...........  ]
-
-
-*/
-
-let brands = ["levis", "puma", "adidas"]
-// console.log("old brands", brands);
-// // console.log("0th index value of brands is " + brands[0]);
-// console.log("1st index", brands[1]);
-// console.log("3st index", brands[3]);
-
-brands[1] = "nike"
-brands[3] = "puma"
-
-// console.log("new_brands", brands);
-// console.log("new_brands", brands[0]);
-
-
-// console.log(brands[2]);
-// console.log(brands[3]);
-
-/* 
-    reading array values => we can use index 
-    mutate/ change /update array values => use index
- 
-*/
-
-
-// let ages = [20, 11, 10, 100, "hundred", undefined, { name: "ram", age: 12 }]
-// console.log(ages);
-
-/* object
-    just like real life objects having different properties
- 
-    <object_name> = {
-        <key | attribute | property > : <value>,
-        <key | attribute | property > : <value>,
-        <key | attribute | property > : <value>,
-    }
- 
+variable
  */
 
+// name = "ram"
+// age = 12
+// active_status = true
+// active_status = false
 
-let bottle = {
-    // "<key>" : "<value>"
-    unit: "cm",
-    height: 15,
-}
+/* data types
+    string
+    number
+    boolean
+*/
+
+var brand = "samsung"
+var color; // color variable // declartion
+color = "red" // initialziation
+color = "white" // re-initialization
 
 
-let projector = {
-    color: "black",
-    model: "vx-100",
-    brand: "samssung"
-}
 
-let person1 = {
-    name: "Ram",
-    age: 12,
-    courses: ["mern", "ux"]
-}
+let name = "ram"
+let age = 10
+let status = true;
 
-let person2 = {
-    name: "Hari",
-    age: 17
-}
-console.log(["nike", "puma"]);
-console.log(projector);
-console.log("the old_brand is ", projector.brand);
-console.log("the oldcolor is ", projector.color);
-projector.color = "white" // mutate/update/  re-initializting object property
-console.log("the new color is ", projector.color);
 
-let number = {
-    "1": "one",
-    2: "one",
-    "one": 1,
-}
 
-console.log(number);
-console.log(number.one);
+/* 
+    ecmascript - js
+    2015 
+*/
 
-let persons = [person1, person2]
-console.log(persons);
+
+
+var unit;
+var unit;
+
+let dimension;
+// let dimension;
+dimension = "3d"
+dimension = "2d"
+
+const PI = 3.14
+// PI = 3.1323123
+
+
+console.log("Hello ")
+let course = "mern class"
+console.log(course);
+
+
+
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,86 @@
+/* varaibales */
+
+
+let name = "Ram"
+
+let age;
+
+/*  */
+age = 12;
+let price = 12.122
+
+let status = true
+
+let brand;
+console.log(brand);
+
+let product_data = null;
+
+/* 
+    string
+    number
+    booolean
+    null
+    undeinfed
+*/
+
+/* 
+    array and object // colelction
+*/
+
+
+let names = ["ram", "hari"]
+let ages = [11, 10]
+
+/* 
+array
+    - usually collection of similar data types
+    - index => always starts with 0
+ */
+
+console.log(names);
+console.log(names[1]);
+names[0] = "shaym "
+console.log(names[0]);
+console.log(names[3]);
+
+
+/*
+    object -> same like real life object 
+    key | property | attribute
+ */
+
+let unit = "cm"
+let wire = {
+    color: "black",
+    length: "121",
+    unit: unit,
+}
+
+console.log(wire.color);
+console.log(wire.black);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
